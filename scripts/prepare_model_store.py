64fa30bc9b0c343d4400534a208f6db0889be7f0
satorikoishi <410453494@qq.com>
2023-12-11 22:28:23 +0800

    mar file

--- scripts/prepare_model_store.py
+++ scripts/prepare_model_store.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 from transformers import AutoModel, AutoConfig, AutoTokenizer
+import subprocess
 
 def get_model_basename(model_name):
     """
@@ -14,6 +15,14 @@ def get_model_basename(model_name):
     """
     return model_name.split("/")[-1]
 
+def get_model_seriesname(model_basename):
+    series_parts = model_basename.split('-')
+    if len(series_parts) > 1:
+        series_name = '-'.join(series_parts[:-1])
+    else:
+        series_name = series_parts[0]
+    return series_name
+
 def download_and_save_model(model_name, save_directory):
     """
     Download a pre-trained model from Hugging Face along with its configuration and tokenizer.
@@ -24,6 +33,8 @@ def download_and_save_model(model_name, save_directory):
     """
     # Create the save directory if it doesn't exist
     os.makedirs(save_directory, exist_ok=True)
+    os.makedirs(os.path.join(save_directory, "config"), exist_ok=True)
+    os.makedirs(os.path.join(save_directory, "model-store"), exist_ok=True)
 
     # Download the configuration
     config = AutoConfig.from_pretrained(model_name)
@@ -37,6 +48,36 @@ def download_and_save_model(model_name, save_directory):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     tokenizer.save_pretrained(save_directory)
     print(f"Model, config, and tokenizer saved in {save_directory}")
+    
+def create_mar_file(model_name, version, model_file, handler_file, extra_files=None, requirements_file=None):
+    """
+    Create a .mar file for TorchServe.
+
+    Parameters:
+    model_name (str): The name of the model.
+    version (str): Model version.
+    model_file (str): Path to the serialized model file (.pt or .pth).
+    handler_file (str): Path to the handler file.
+    extra_files (str, optional): Comma-separated paths to extra files needed by the model.
+    requirements_file (str, optional): Path to the requirements.txt file.
+    """
+    mar_command = [
+        "torch-model-archiver",
+        "--model-name", model_name,
+        "--version", version,
+        "--model-file", model_file,
+        "--handler", handler_file
+    ]
+
+    if extra_files:
+        mar_command.extend(["--extra-files", extra_files])
+    
+    if requirements_file:
+        mar_command.extend(["--requirements-file", requirements_file])
+
+    print(f"Running {mar_command} to generate .mar file")
+    # Create the .mar file
+    subprocess.run(mar_command)
 
 def main():
     parser = argparse.ArgumentParser(description="Download a Hugging Face model with its config and tokenizer.")
@@ -45,9 +86,17 @@ def main():
     args = parser.parse_args()
     model_name = args.model_name
     model_basename = get_model_basename(model_name)
+    model_seriesname = get_model_seriesname(model_basename)
     save_directory = os.path.join(os.path.dirname(__file__), f"../model_archive/{model_basename}")
+    handler_dir = os.path.join(os.path.dirname(__file__), f"../model_archive/handler")
+    extra_json_files = ["config.json", "special_tokens_map.json", "tokenizer.json", "tokenizer_config.json"]
 
     download_and_save_model(model_name, save_directory)
-
+    create_mar_file(model_basename, "1.0", os.path.join(save_directory, "pytorch_model.bin"), os.path.join(handler_dir, f"{model_seriesname}_handler.py"),
+        extra_files=','.join([os.path.join(save_directory, j) for j in extra_json_files]),
+    )
+    # Move to model store
+    subprocess.run(f"mv {model_basename}.mar {os.path.join(save_directory, 'model-store')}", shell=True)
+    
 if __name__ == "__main__":
     main()
